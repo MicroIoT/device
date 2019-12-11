11a76ca1061336cad6ae45e36cc99bc436ff3431
caoxinyu <caoxinyu@gmail.com>
2019-12-11 12:35:25 +0800

    fix action bug

--- src/components/stomp.js
+++ src/components/stomp.js
@@ -81,8 +81,11 @@ class StompClient {
       let request = JSON.parse(msg.body)
       let resp = store.getters.getActionResponse(request.action)
       let topic = resultTopic + 'action.' + deviceId + '.' + request.requestId
+      let actionType = store.getters.getCurrentUser.deviceType.actionTypes[request.action].request
+      let requestName = Object.keys(actionType)[0]
+      let type = actionType[requestName].dataType
       if (resp) {
-        let value = toDataValue(request.action, request.value)
+        let value = toDataValue(request.action, request.value, type)
         store.commit('setActionRequest', value)
         let response = {}
         response.value = resp
